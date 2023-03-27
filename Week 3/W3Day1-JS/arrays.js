47efa5c88ba0f201cac849ba129bd451b99c9ad7
FiendRiver <fiendriver@gmail.com>
2023-03-27 21:07:20 +0300

    did exercises

---
+++ Week 3/W3Day1-JS/arrays.js
@@ -0,0 +1,26 @@
+// Arrays 
+// Data Structures // Objects 
+
+
+
+let arr = ['Apple', 'Banana', 'Kiwi'];
+let arr1 = new Array();
+console.log(arr, arr1)
+
+//console.log(arr)
+// arr[0] = 'Orange';
+// console.log(typeof(arr));
+console.log(arr[1][1]);
+
+
+
+//join (' ')  (joins items in an array and returns a string)
+// let str = str.join
+
+
+// pop  (removes the last element from an array and returns that removed element)
+
+// push (pushes an element to the end of an array )
+
+// shift (removes the first element and returns that element. reverse of pop. sort of.)
+
